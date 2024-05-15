cef9224c0b5afbaf73c14149cf3343c60bbc91e4
Jameel Ursonal <londoholm1@gmail.com>
2024-05-15 08:35:27 +0800

    removed  erroneous elements

--- src/components/ui/balloon.tsx
+++ src/components/ui/balloon.tsx
@@ -1,85 +1,86 @@
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useEffect, useState } from "react";
 
 function Balloon() {
-  // document.body.style.backgroundColor = "#1f2830";
+  document.body.style.backgroundColor = "#1f2830";
 
-  // const [isTasksOpen, setIsTasksOpen] = useState(false);
-  // const [taskList, setTaskList] = useState<JSX.Element[]>([]);
-  // const [oneList, setoneList] = useState<JSX.Element[]>([]);
-  // const [twoList, settwoList] = useState<JSX.Element[]>([]);
-  // const [threeList, setthreeList] = useState<JSX.Element[]>([]);
-  // const [fourList, setfourList] = useState<JSX.Element[]>([]);
+  const [isTasksOpen, setIsTasksOpen] = useState(false);
+  const [taskList, setTaskList] = useState<JSX.Element[]>([]);
+  const [oneList, setoneList] = useState<JSX.Element[]>([]);
+  const [twoList, settwoList] = useState<JSX.Element[]>([]);
+  const [threeList, setthreeList] = useState<JSX.Element[]>([]);
+  const [fourList, setfourList] = useState<JSX.Element[]>([]);
 
-  // //add random task button function
-  // const onAddRandomTask = () => {
-  //   let rvalue = Math.floor(Math.random() * 100) + 1;
-  //   let newTask = (
-  //     <div className={Math.random() < 0.05 ? "redtaskvalue" : "taskvalue"}>{rvalue}</div>
-  //   );
-  //   setTaskList((prevTask) => [...prevTask, newTask]);
-  // };
+  //add random task button function
+  const onAddRandomTask = () => {
+    let rvalue = Math.floor(Math.random() * 100) + 1;
+    let newTask = (
+      <div className={Math.random() < 0.05 ? "redtaskvalue" : "taskvalue"}>{rvalue}</div>
+    );
+    setTaskList((prevTask) => [...prevTask, newTask]);
+  };
 
-  // //admit task button function
-  // const onAdmitTask = () => {
-  //   let twoListSum = twoList.reduce((sum, value) => sum + value.props.children, 0);
-  //   let threeListSum = threeList.reduce((sum, value) => sum + value.props.children, 0);
-  //   let fourListSum = fourList.reduce((sum, value) => sum + value.props.children, 0);
+  //admit task button function
+  const onAdmitTask = () => {
+    let twoListSum = twoList.reduce((sum, value) => sum + value.props.children, 0);
+    let threeListSum = threeList.reduce((sum, value) => sum + value.props.children, 0);
+    let fourListSum = fourList.reduce((sum, value) => sum + value.props.children, 0);
 
-  //   if (taskList.length > 0) {
-  //     setTaskList((prevTaskList) => prevTaskList.slice(1));
+    if (taskList.length > 0) {
+      setTaskList((prevTaskList) => prevTaskList.slice(1));
 
-  //     if (taskList[0].props.className === "redtaskvalue") {
-  //       setoneList((prevOneList) => [...prevOneList, taskList[0]]);
-  //     } else {
-  //       if (fourListSum <= threeListSum && fourListSum <= twoListSum) {
-  //         setfourList((prevFourList) => [...prevFourList, taskList[0]]);
-  //       } else if (threeListSum <= twoListSum) {
-  //         setthreeList((prevThreeList) => [...prevThreeList, taskList[0]]);
-  //       } else {
-  //         settwoList((prevTwoList) => [...prevTwoList, taskList[0]]);
-  //       }
-  //     }
-  //   }
-  // };
+      if (taskList[0].props.className === "redtaskvalue") {
+        setoneList((prevOneList) => [...prevOneList, taskList[0]]);
+      } else {
+        if (fourListSum <= threeListSum && fourListSum <= twoListSum) {
+          setfourList((prevFourList) => [...prevFourList, taskList[0]]);
+        } else if (threeListSum <= twoListSum) {
+          setthreeList((prevThreeList) => [...prevThreeList, taskList[0]]);
+        } else {
+          settwoList((prevTwoList) => [...prevTwoList, taskList[0]]);
+        }
+      }
+    }
+  };
 
-  // const onInflateBalloon = () => {
-  //   onAddRandomTask();
-  //   onAdmitTask();
-  // };
+  const onInflateBalloon = () => {
+    onAddRandomTask();
+    onAdmitTask();
+  };
 
-  // const onOpenTasks = () => {
-  //   setIsTasksOpen(!isTasksOpen);
-  // };
-  // return (
-  //   <div>
-  //     {/* part 2 */}
-  //     <Button
-  //       className="absolute bottom-7 left-1/2 transform translate-x-1/2"
-  //       onClick={onInflateBalloon}
-  //     />
+  const onOpenTasks = () => {
+    setIsTasksOpen(!isTasksOpen);
+  };
+  return (
+    <div>
+      {/* part 2 */}
+      <Button
+        className="absolute bottom-7 left-1/2 transform translate-x-1/2"
+        onClick={onInflateBalloon}
+      />
 
-  //     {/* part 1 */}
-  //     <div className="absolute right-7 top-1/2 transform translate-y-1/2">
-  //       {isTasksOpen ? <ChevronLeft size={18} onClick={onOpenTasks} /> : <ChevronRight size={18} />}
-  //     </div>
+      {/* part 1 */}
+      <div className="absolute right-7 top-1/2 transform translate-y-1/2">
+        {isTasksOpen ? <ChevronLeft size={18} onClick={onOpenTasks} /> : <ChevronRight size={18} />}
+      </div>
 
-  //     {/* <div className="flex flex-col p-3 gap-2">
-  //       <div>
-  //         <QueueGrid heading="High Priority Queue 1" tasklist={oneList} setTaskList={setoneList} />
-  //       </div>
-  //       <div>
-  //         <QueueGrid heading="Regular Queue 2" tasklist={twoList} setTaskList={settwoList} />
-  //       </div>
-  //       <div>
-  //         <QueueGrid heading="Regular Queue 3" tasklist={threeList} setTaskList={setthreeList} />
-  //       </div>
-  //       <div>
-  //         <QueueGrid heading="Regular Queue 4" tasklist={fourList} setTaskList={setfourList} />
-  //       </div>
-  //     </div> */}
-  //   </div>
-  return <>hh</>;
-  // );
+      {/* <div className="flex flex-col p-3 gap-2">
+        <div>
+          <QueueGrid heading="High Priority Queue 1" tasklist={oneList} setTaskList={setoneList} />
+        </div>
+        <div>
+          <QueueGrid heading="Regular Queue 2" tasklist={twoList} setTaskList={settwoList} />
+        </div>
+        <div>
+          <QueueGrid heading="Regular Queue 3" tasklist={threeList} setTaskList={setthreeList} />
+        </div>
+        <div>
+          <QueueGrid heading="Regular Queue 4" tasklist={fourList} setTaskList={setfourList} />
+        </div>
+      </div> */}
+    </div>
+  );
 }
 
 interface QueueGridProps {
@@ -124,39 +125,8 @@ function QueueGrid(props: QueueGridProps) {
     };
   }, [progress, setTaskList, tasklist]);
 
-  //LineProgress Customization (colors)
-  // const theme = createTheme({
-  //   palette: {
-  //     primary: {
-  //       main: "#388e3c",
-  //     },
-  //     secondary: {
-  //       main: "#1565c0",
-  //     },
-  //   },
-  // });
-
-  // const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-  //   height: 12,
-  //   borderRadius: 1,
-  //   [`&.${linearProgressClasses.colorPrimary}`]: {
-  //     backgroundColor: theme.palette.mode === "light" ? "#9FA4A9" : 500,
-  //   },
-  //   [`& .${linearProgressClasses.bar}`]: {
-  //     backgroundColor: theme.palette.mode === "light" ? "#982649" : "#1565c0",
-  //   },
-  // }));
-  //end of LineProgress Customization
-
   return (
-    <div
-    // sx={{
-    //   height: "auto",
-    //   padding: 1,
-    //   borderColor: heading[heading.length - 1] === "1" ? "#bf1733" : "black",
-    //   backgroundColor: heading[heading.length - 1] === "1" ? "#4d3636" : "#2e3942",
-    // }}
-    >
+    <div>
       <h3>{heading}</h3>
       <p>Queue List:</p>
       <div className="subtask">
@@ -165,9 +135,6 @@ function QueueGrid(props: QueueGridProps) {
         ))}
       </div>
       <p>Duration</p>
-      {/* <ThemeProvider theme={theme}>
-        <BorderLinearProgress variant="determinate" color="primary" value={normalise(progress)} />
-      </ThemeProvider> */}
     </div>
   );
 }
